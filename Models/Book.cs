2ef8c95168925b48fd52219b8f537ad8e93cae21
malsabi <xxdarklord65xx@gmail.com>
2023-06-19 07:42:05 +0400

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int? PublicationYear { get; set; }
+    }
+}
---
+++ ViewModels/BooksViewModel.cs
@@ -0,0 +1,56 @@
+﻿using BookStoreApp.Views;
+using BookStoreApp.Models;
+using BookStoreApp.Messages;
+using BookStoreApp.Services;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BookStoreApp.ViewModels
+{
+    public class BooksViewModel : ObservableRecipient, IRecipient<InsertNewBookMessage>
+    {
+        private readonly IBookService bookService;
+
+        public ObservableCollection<Book> BooksList { get; } = new ObservableCollection<Book>();
+
+        public IRelayCommand NavigateToAddBookCommand { get; }
+
+        public BooksViewModel(IBookService bookService)
+        {
+            this.bookService = bookService;
+
+            if (bookService.GetBooks() is List<Book> books)
+            {
+                foreach (Book book in books)
+                {
+                    BooksList.Add(book);
+                }
+            }
+
+            NavigateToAddBookCommand = new RelayCommand(NavigateToAddBook);
+            IsActive = true;
+        }
+
+        private async void NavigateToAddBook()
+        {
+            await Shell.Current.GoToAsync(nameof(AddBookPage), true);
+        }
+
+        protected override void OnActivated()
+        {
+            WeakReferenceMessenger.Default.Register(this);
+        }
+
+        protected override void OnDeactivated()
+        {
+            WeakReferenceMessenger.Default.Unregister<InsertNewBookMessage>(this);
+        }
+
+        public void Receive(InsertNewBookMessage message)
+        {
+            BooksList.Add(message.Value);
+        }
+    }
+}
